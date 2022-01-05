7fc034c0dce8380a5c4f0925c5847775c43e7448
ashish-panicker <ashish.s.panicker@outlook.com>
2022-01-05 16:13:18 +0530

    serlization and assignment 6

--- README.md
+++ README.md
@@ -185,3 +185,74 @@ Create a java program with a method call `largestInArray`.
 - The mothod should accept a integer array.
 - It shoudl then find the largest number in the array.
 - The it shoudl return the number back to the calling method.
+
+
+### Assignment 6 - OrderProcessing
+
+Create a java program with the following features.
+
+- This will be a menu driven program with `3` options.
+  
+  ```
+  1. Process an order
+  2. View all orders
+  3. Exit
+  ```
+- Oprion 1 would ask the users to provide the item details. Once an item is entered a promt will ask the user if they want to add another item. If answer is `Y` then the user will be prompted to enter the item details.
+- The program must have a class called `OrderProcessing`.
+- The program will have a class called `Order` for repreenting the order.
+- Order class will have the following attributes.
+  
+  ```
+  Order Id
+  Customer Name
+  Item List
+  Net Price
+  ```
+
+- The program will have a class called `Item` for repreenting the items in the order.
+- Item class will have the following attributes
+
+    ```
+    Item Code
+    Item Name
+    Unit Price
+    Quantity
+    Total Price 
+    ```
+  
+
+- The order and item detaisl must be collected from the user.
+- The order id must be uniquely generated.
+- Once the items are completely entered the order must be processed and it must printed into the screen.
+- Once the order is processed the order must be saved into a file.
+- The file must be created inside the `orders` folder.
+- The file name must be int the following format `order-#order_id`.
+- Orders must be printed in a tabular format
+```
+--------------------------------------------------------------------------
+ Order Id:  1                                   Customer:    Ashish         
+ 
+ Item Name             Qty     Unit Price     Total Price
+ --------------------------------------------------------------------------
+  Book - Animal Farm    1       72.99           72.99 
+         Soap - Dove    2       44.99           89.98 
+ Noodles - Spaghetti    3       23.99           71.97 
+  Softdrinks - Pepsi    3       33.99           101.97 
+        Flour - Atta    1       23.99           23.99 
+---------------------------------------------------------------------------
+ Net Total: 199.95 
+
+```
+- Option 2 will generate an output in the following format.
+  
+    ```
+    Order Id          File name           Modified date
+    -----------------------------------------------------------------------
+    1                order-1              2020-01-01 12:00:00
+    2                order-2              2020-01-02 12:00:00
+    -----------------------------------------------------------------------
+
+    Enter the order number you want to view
+
+    ```
--- javademos/generics/OrderProcessor.java
+++ javademos/generics/OrderProcessor.java
@@ -1,5 +1,14 @@
 package javademos.generics;
 
+import java.beans.Transient;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,29 +16,75 @@ public class OrderProcessor {
 
     public static void main(String[] args) {
 
-        List<Item> items = new ArrayList<>();
+        // List<Item> items = new ArrayList<>();
 
-        items.add(new Item("Book - Animal Farm", 1, 72.99));
-        items.add(new Item("Soap - Dove", 2, 44.99));
-        items.add(new Item("Noodles - Spaghetti", 3, 23.99));
-        items.add(new Item("Softdrinks - Pepsi", 3, 33.99));
-        items.add(new Item("Flour - Atta", 1, 23.99));
+        // items.add(new Item("Book - Animal Farm", 1, 72.99));
+        // items.add(new Item("Soap - Dove", 2, 44.99));
+        // items.add(new Item("Noodles - Spaghetti", 3, 23.99));
+        // items.add(new Item("Softdrinks - Pepsi", 3, 33.99));
+        // items.add(new Item("Flour - Atta", 1, 23.99));
 
-        Order order = new Order(1, "Ashish", items);
-        order.displayOrder();
-        
+        // Order order = new Order(1, "Ashish", items);
+        // order.displayOrder();
+        // OrderFileProcessor.serializeOrder(order);
+
+        OrderFileProcessor.deSerializeOrder();
     }
 
 }
 
 
-class Order {
+final class OrderFileProcessor {
+
+    public static void deSerializeOrder(){
+        try (ObjectInputStream oin = new ObjectInputStream(new FileInputStream("orders_ser"))) {
+            Order order = (Order) oin.readObject();
+            order.displayOrder();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void serializeOrder(Order order) {
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("orders_ser"))) {
+            oos.writeObject(order);            
+        } catch (IOException e) {
+            System.err.println("Serialization IO Error:" + e.getMessage());
+        }
+    }
+
+    public static void processOrder(Order order) {
+        try (PrintWriter out = new PrintWriter("orders")) {
+            out.printf("Order Id: %d  %50s %n", order.getOrderId(), order.getCustomer());
+            out.printf("%50s %n", "Items");
+            for (Item item : order.getItems()) {
+                out.printf("%20s %d %4.2f %4.2f %n", item.getName(), item.getQuantity(),
+                        item.getPrice(), item.getTotalCost());
+            }
+            out.printf(
+                    "============================================================================%n");
+            out.printf("Net Total: %4.2f %n", order.getNetTotal());
+
+        } catch (IOException e) {
+            System.err.println("File IO Error:" + e.getMessage());
+        } finally {
+            System.out.println("Order processed successfully");
+        }
+    }
+
+}
+
+
+class Order implements Serializable {
 
     private int orderId;
     private String customer;
     // items
     private List<Item> items;
     private double netTotal;
+
+    private transient LocalDate modifedDate;
 
     public Order(int orderId, String customer, List<Item> items) {
         this.orderId = orderId;
@@ -39,6 +94,7 @@ class Order {
         for (Item item : items) {
             netTotal += item.getPrice();
         }
+        this.modifedDate = LocalDate.now();
     }
 
     public int getOrderId() {
@@ -73,14 +129,24 @@ class Order {
         this.netTotal = netTotal;
     }
 
+    public LocalDate getModifedDate() {
+        return modifedDate;
+    }
+
+    public void setModifedDate(LocalDate modifedDate) {
+        this.modifedDate = modifedDate;
+    }
+
     public void displayOrder() {
-        System.out.printf("Order Id: %d  %50s %n", orderId,  customer);
+        System.out.printf("Order Id: %d  %50s %n", orderId, customer);
         System.out.printf("%50s %n", "Items");
         for (Item item : items) {
-            System.out.printf("%20s %d %4.2f %4.2f %n", 
-                item.getName(), item.getQuantity(), item.getPrice(), item.getTotalCost());
+            System.out.printf("%20s %d %4.2f %4.2f %n", item.getName(), item.getQuantity(),
+                    item.getPrice(), item.getTotalCost());
         }
-        System.out.printf("============================================================================%n");
+        System.out.printf(
+                "============================================================================%n");
+        System.out.printf("Modified Date: %s %n", modifedDate.toString());
         System.out.printf("Net Total: %4.2f %n", netTotal);
 
     }
@@ -89,7 +155,7 @@ class Order {
 }
 
 
-class Item {
+class Item implements Serializable {
 
     private String name;
     private int quantity;
